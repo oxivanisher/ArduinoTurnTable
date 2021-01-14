09d1f0fa386ddb8350e8ef772c2b59bb80a86093
Marc Urben <aegnor@mittelerde.ch>
2021-01-15 00:20:56 +0100

    pushing work ... it works but direction change not

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Platform.io version of https://docs.platformio.org/page/projectconf.html
 
 **This is not my work, I am only porting the code to VScode with PlatformIo.**
+
+
+## Notes on the LiquidCrystal library
+The version is using the LiquidCrystal library from
+[Drone Mesh - DIY Wireless Youtube Subscriber Counter 2018 // ESP8266 Subscriber Counter](https://www.youtube.com/watch?v=yfDWCga7iP4).
+This lib is included in the `lib/` folder and I had to fix the path in the `I2CIO.cpp` file to the used `Wire` lib. This lib was added directly to the project, since it was difficult to find the correct one, working with the Display which has the I2C attached to it.
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 #include <Stepper.h>
-#include <LiquidCrystal.h>
 
-// based on https://www.instructables.com/28BYJ-48-Stepper-Motor-Control-System-Based-On-Ard/
+// based on
+// https://www.instructables.com/28BYJ-48-Stepper-Motor-Control-System-Based-On-Ard/
 
 int Iint1 = 0;
 int Iint2 = 1;
@@ -10,66 +11,122 @@ int anjian1 = 2;
 int anjian2 = 3;
 int motorSpeed;
 
-LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
+// don't update the speed all the time
+unsigned long next_update;
+const int update_every = 100;
 
-const int stepsPerRevolution = 200;
+// button variables and debounce
+bool cw = true;
+bool pause = true;
+unsigned long lastCwDebounceTime = 0;
+unsigned long lastPauseDebounceTime = 0;
+const int debounceDelay = update_every;
+
+// int step = 0;
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3,
+                      POSITIVE);  // Set the LCD I2C address
+
+// const int stepsPerRevolution = 200;
+const int stepsPerRevolution = 2038;
+// const int stepsPerRevolution = 2048;
+// const int stepsPerRevolution = 768;
+// const int stepsPerRevolution = 4095;
+// const int stepsPerRevolution = 48;
 
 // Here set the stepper motor rotation step how much is a circle
-int dim = stepsPerRevolution;
+// int dim = stepsPerRevolution;
 
 // Set the step motor number and pin
-Stepper myStepper(stepsPerRevolution, 10, 11, 12, 13);
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
-void clockwise()
-{
-  // clockwise rotation
-  dim = stepsPerRevolution;
-  lcd.setCursor(10, 1);
-  lcd.print(">>>>>>");
+void changeDirection() {
+  if (lastCwDebounceTime < millis()) {
+    if (cw) {
+      cw = false;
+    } else {
+      cw = true;
+    }
+    lastCwDebounceTime = millis() + debounceDelay;
+  }
 }
 
-void counterclockwise()
-{
-  // anti-clockwise
-  dim = -stepsPerRevolution;
-  lcd.setCursor(10, 1);
-  lcd.print("<<<<<<");
+void togglePause() {
+  if (lastPauseDebounceTime < millis()) {
+    if (pause) {
+      pause = false;
+    } else {
+      pause = true;
+    }
+    lastPauseDebounceTime = millis() + debounceDelay;
+  }
 }
 
-void setup()
-{
+void setup() {
+  // Serial.begin(9600);
+
   lcd.begin(16, 2);
-  lcd.print("speed:");
-  lcd.setCursor(10, 0);
-  lcd.print("n/min");
+  lcd.setCursor(0, 0);
+  lcd.print("Speed      u/min");
+  // lcd.setCursor(11, 0);
+  // lcd.print("u/min");
   lcd.setCursor(0, 1);
-  lcd.print("Direction:");
+  lcd.print("Direction ------");
 
   // Set the motor speed of 60 steps per minute
-  myStepper.setSpeed(60);
+  myStepper.setSpeed(10);
   pinMode(anjian1, INPUT_PULLUP);
   pinMode(anjian2, INPUT_PULLUP);
-  attachInterrupt(Iint1, counterclockwise, FALLING);
-  attachInterrupt(Iint2, clockwise, FALLING);
-  Serial.begin(9600);
+  attachInterrupt(Iint1, changeDirection, FALLING);
+  attachInterrupt(Iint2, togglePause, FALLING);
 }
 
-void loop()
-{
-  myStepper.step(dim);
-  void Direction();
+void loop() {
+  // if (!pause) {
+  //   if (cw) {
+  //     myStepper.step(stepsPerRevolution / 100);
+  //   } else {
+  //     myStepper.step(stepsPerRevolution / -100);
+  //   }
+  // }
+
+  // if (millis() > next_update) {
+  //   next_update = millis() + update_every;
 
   // Read the sensor values:
   int sensorReading = analogRead(A0);
 
-  // Map it to a range of 0-150:
-  int motorSpeed = map(sensorReading, 0, 1023, 0, 150);
+  // Map it to a range of 0-100:
+  int motorSpeed = map(sensorReading, 1023, 0, 1, 10);
+  // Serial.print("motorSpeed: ");
+  // Serial.println(motorSpeed);
 
   // Set the motor speed:
-  if (motorSpeed > 0)
-  {
-    myStepper.setSpeed(motorSpeed);
-    lcd.setCursor(6, 0);
-    lcd.print(float(float(motorSpeed) / float(200)));
+  // if (motorSpeed > 0) {
+  myStepper.setSpeed(motorSpeed);
+  lcd.setCursor(6, 0);
+  // lcd.print(float(float(motorSpeed) / float(stepsPerRevolution)));
+  lcd.print(motorSpeed);
+
+  lcd.setCursor(10, 1);
+  // lcd.setCursor(0, 1);
+  if (pause) {
+    lcd.print("||||||");
+  } else {
+    if (cw) {
+      lcd.print(">>>>>>");
+      myStepper.step(stepsPerRevolution / 100);
+      // lcd.print("Direction >>>>>>");
+    } else {
+      lcd.print("<<<<<<");
+      myStepper.step(stepsPerRevolution / -100);
+      // lcd.print("Direction <<<<<<");
+    }
+    // }
+
+    // Serial.print("speed: ");
+    // Serial.print(float(float(motorSpeed) / float(200)));
+    // Serial.println(" n/min");
+    // }
   }
 }
